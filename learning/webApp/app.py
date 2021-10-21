6221bd815c50fcd9f35f5dcfcb2194f21bf78485
unknown <donalhowe@gmail.com>
2021-10-21 11:15:48 +0100

    cathicng up on wednesdays class2

--- learning/webApp/app.py
+++ learning/webApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request,render_template
 
 import datetime
 
@@ -13,5 +13,20 @@ def index():
 def hello():
     return "yolo swagger"
 
+@app.get("/showform")
+def display_form():
+    return render_template("form.html")
+@app.get("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.post("/processform")
+def process_form():
+    the_name=request.form["theName"]
+    the_dob=request.form["thedob"]
+    return f"HI There,{the_name}, we know you were born on the {the_dob} "   
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
